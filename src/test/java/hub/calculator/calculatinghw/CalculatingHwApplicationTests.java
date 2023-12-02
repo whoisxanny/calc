b1c1c2d0c2f0bc94c03d80de7a00c9bb2ea92b08
whoisxanny <tuffx4nny@gmail.com>
2023-12-02 15:40:57 +0500

    Initial Changes

---
+++ src/test/java/hub/calculator/calculatinghw/CalculatingHwApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.calculator.calculatinghw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatingHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
